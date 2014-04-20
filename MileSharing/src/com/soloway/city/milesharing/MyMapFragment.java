39f1732c34033bab865cd793f77310789e81aac8
spacesoldier <kibersoldier@gmail.com>
2014-04-21 00:12:08 +0400

    package name changed
    droiddevlab to soloway.city

---
+++ MileSharing/src/com/soloway/city/milesharing/MyMapFragment.java
@@ -0,0 +1,32 @@
+package com.soloway.city.milesharing;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.soloway.city.milesharing.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+public class MyMapFragment extends Fragment {
+
+    private GoogleMap map;
+
+    public GoogleMap getMap() {
+		return map;
+	}
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_map, null, false);
+
+//        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.container))
+//            .getMap();
+//
+
+        return v;
+    }
+}
---
+++ MileSharing/src/com/soloway/city/milesharing/Point.java
@@ -0,0 +1,9 @@
+package com.soloway.city.milesharing;
+
+public class Point {
+	 String mName;
+	 String mDescription;
+	 String mIconUrl;
+	 double mLatitude;
+	 double mLongitude;
+	}
